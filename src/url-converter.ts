09f7540e2eb4d4fbc4b2e9e56003314625c8f01d
alwxkxk <alwxkxk@gmail.com>
2019-04-28 13:56:30 +0800

    url-converter:add multi files,filter inclueds

--- src/url-converter.ts
+++ src/url-converter.ts
@@ -5,10 +5,21 @@ import * as URL from "url";
 const rootPath = process.cwd();
 const outputDir = path.join(rootPath, "output");
 
+interface options {
+  filter?: {
+    includes: string[];
+  }
+  multi?: boolean;
+  saveSource?: boolean;
+
+}
+
 class Converter {
   private urlArray: string[];
+  private options: options;
 
-  public start(inputFile: string) {
+  public start(inputFile: string, options?:options) {
+    this .options = options || {};
     return this .parseInput(inputFile);
   }
 
@@ -18,7 +29,8 @@ class Converter {
    * @param {string} text
    * @memberof Converter
    */
-  public fetchUrl(text: string) {
+  public fetchUrl(text: string, append?:boolean) {
+    // TODO:filter
     const urlReg = /http[s]?:\/{2}(?:[\/-\w.]|(?:%[\da-fA-F]{2}))+/gm;
     const set = new Set();
     let match;
@@ -26,12 +38,26 @@ class Converter {
       match = urlReg.exec(text);
       if (match) {
         const urlString: string = match[0];
-        set.add(urlString);
+        // filter includes
+        if(this .options.filter && this .options.filter.includes){
+          this .options.filter.includes.forEach((v)=>{
+            if(urlString.includes(v)){
+              set.add(urlString);
+            }
+          })
+        }
+        else{
+          set.add(urlString);
+        }
         // console.log(urlString);
       }
     } while (match);
-
-    this .urlArray = Array.from(set);
+    if(!append || !this .urlArray){
+      this .urlArray = Array.from(set);
+    }
+    else{
+      this .urlArray= this .urlArray.concat(Array.from(set));
+    }
     // long string first
     this .urlArray.sort((a, b) => {
       return -(a.length - b.length);
@@ -62,11 +88,23 @@ class Converter {
       // console.error("input is null.");
       return Promise.reject("input is null.");
     }
-    const inputFile = path.join(rootPath, fileName);
-    const urlsFile = path.join(outputDir, "source_" + fileName);
-    const outputFile = path.join(outputDir, fileName);
-    const text = fs.readFileSync(inputFile, "utf-8");
 
+    let text;
+    let inputFile = path.join(rootPath, fileName);
+    let outputFile = path.join(outputDir, fileName);
+
+    if(this .options.multi){
+      // read all file and fetchUrl
+      await this .readFiles(fileName)
+      // console.log("readFiles complete.");
+    }
+    else{
+      // only one file.
+
+      text = fs.readFileSync(inputFile, "utf-8");
+      this .fetchUrl(text);
+    }
+    // output
     try {
       // delete old output
       await fs.remove(outputDir);
@@ -74,9 +112,10 @@ class Converter {
       await fs.ensureDir(outputDir);
 
       // copy inputFile into output directory
-      fs.copy(inputFile, urlsFile);
+      if(this .options.saveSource){
+        fs.copy(inputFile, path.join(outputDir, "source_" + fileName));
+      }
       // fetch url and save the urls.json
-      this .fetchUrl(text);
       fs.outputJson(path.join(outputDir, "urls.json"), this .urlArray);
       // replace url and save the output
       const output = this .replaceUrl(text);
@@ -88,9 +127,41 @@ class Converter {
       return Promise.reject(error);
     }
   }
+
+  async readFiles(dir:string) {
+    const scope = this ;
+    // console.log("readFiles", dir);
+    return fs.readdir(dir)
+    .then(async (filesName)=>{
+      for(let i = 0;i< filesName.length;i++){
+        let fileName = filesName[i];
+        let p = path.join(dir, fileName)
+        let stat = fs.statSync(p);
+        // console.log(fileName, stat.isDirectory());
+        if(stat.isDirectory()){
+          // console.log(fileName, "is directory.");
+          await scope.readFiles(p);
+        }
+        else{
+          await fs.readFile(p, 'utf-8')
+          .then((content)=>{
+            // console.log(fileName, "is file");
+            scope.fetchUrl(content, true);
+          })
+        }
+      }
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  }
 }
 
 export default Converter;
 /* tslint:disable */
 // const test = new Converter();
 // test.start("package_esp8266com_index.json");
+
+// hexo posts: multi files
+const test = new Converter();
+test.start("_posts",{multi:true,filter:{includes:["sinaimg"]}});
